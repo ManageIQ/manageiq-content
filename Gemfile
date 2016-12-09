a08378a42af3681fa8874e21fb9e59061528f01a
Brandon Dunne <bdunne@redhat.com>
2016-12-09 10:19:16 -0500

    Get specs running

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
 gemspec
+
+# Load Gemfile with dependencies from manageiq
+eval_gemfile(File.expand_path("spec/manageiq/Gemfile", __dir__))
+
+gem "codeclimate-test-reporter", :require => false
---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'manageiq-content'
+
+def require_domain_file
+  spec_name = caller_locations.first.path
+  file_name = spec_name.sub("spec/", "").sub("_spec.rb", ".rb")
+
+  require file_name
+end
+
+RSpec.configure do |config|
+  config.include Spec::Support::AutomationHelper
+
+  config.before(:suite) do
+    puts "** Resetting #{ENV["AUTOMATE_DOMAINS"]} domain(s)"
+    Tenant.seed
+    MiqAeDatastore.reset
+    MiqAeDatastore.reset_to_defaults
+  end
+
+  config.after(:suite) do
+    MiqAeDatastore.reset
+  end
+end
