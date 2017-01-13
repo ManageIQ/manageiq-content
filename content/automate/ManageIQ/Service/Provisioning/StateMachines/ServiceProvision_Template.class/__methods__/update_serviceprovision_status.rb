78ffe23931ed9bad72ea00733c8808f5cf129875
Patrik Kománek <pkomanek@redhat.com>
2017-01-13 15:25:26 +0100

    Refactoring Service update_serviceprovision_status.

--- content/automate/ManageIQ/Service/Provisioning/StateMachines/ServiceProvision_Template.class/__methods__/update_serviceprovision_status.rb
+++ content/automate/ManageIQ/Service/Provisioning/StateMachines/ServiceProvision_Template.class/__methods__/update_serviceprovision_status.rb
@@ -2,28 +2,57 @@
 # Description: This method updates the service provision status.
 # Required inputs: status
 #
+module ManageIQ
+  module Automate
+    module Service
+      module Provisioning
+        module StateMachines
+          module ServiceProvision_Template
+            class UpdateServiceProvisionStatus
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-prov = $evm.root['service_template_provision_task']
+              def main
+                prov = @handle.root['service_template_provision_task']
 
-unless prov
-  $evm.log(:error, "service_template_provision_task object not provided")
-  exit(MIQ_STOP)
+                if prov.nil?
+                  @handle.log(:error, "service_template_provision_task object not provided")
+                  raise "service_template_provision_task object not provided"
+                end
+
+                updated_message = update_status_message(prov, @handle.inputs['status'])
+
+                if @handle.root['ae_result'] == "error"
+                  @handle.create_notification(:level   => "error",
+                                              :subject => prov.miq_request,
+                                              :message => "Service Provision Error: #{updated_message}")
+                end
+              end
+
+              private
+
+              def update_status_message(prov, status)
+                updated_message  = "Server [#{@handle.root['miq_server'].name}] "
+                updated_message += "Service [#{prov.destination.name}] "
+                updated_message += "Step [#{@handle.root['ae_state']}] "
+                updated_message += "Status [#{status}] "
+                updated_message += "Message [#{prov.message}] "
+                updated_message += "Current Retry Number [#{@handle.root['ae_state_retries']}]"\
+                                    if @handle.root['ae_result'] == 'retry'
+                prov.miq_request.user_message = updated_message
+                prov.message = status
+
+                updated_message
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
-
-# Get status from input field status
-status = $evm.inputs['status']
-
-# Update Status Message
-updated_message  = "Server [#{$evm.root['miq_server'].name}] "
-updated_message += "Service [#{prov.destination.name}] "
-updated_message += "Step [#{$evm.root['ae_state']}] "
-updated_message += "Status [#{status}] "
-updated_message += "Message [#{prov.message}] "
-updated_message += "Current Retry Number [#{$evm.root['ae_state_retries']}]" if $evm.root['ae_result'] == 'retry'
-prov.miq_request.user_message = updated_message
-prov.message = status
-
-if $evm.root['ae_result'] == "error"
-  $evm.create_notification(:level => "error", :subject => prov.miq_request, \
-                           :message => "Service Provision Error: #{updated_message}")
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Service::Provisioning::StateMachines::
+    ServiceProvision_Template::UpdateServiceProvisionStatus.new.main
 end
