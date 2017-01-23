b9ff1c681477e8c6ce42baffab743536d12cde7e
william fitzgerald <wfitzger@redhat.com>
2017-01-23 15:19:25 -0500

    Automate - Refactoring infra vm pre_retirement method.

--- content/automate/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/pre_retirement.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/pre_retirement.rb
@@ -1,10 +1,41 @@
+# / Infra / VM / Retirement / StateMachines / PreRetirement
+
 #
 # Description: This method powers-off the VM on the provider
 #
 
-vm = $evm.root['vm']
-unless vm.nil? || vm.attributes['power_state'] == 'off'
-  ems = vm.ext_management_system
-  $evm.log('info', "Powering Off VM <#{vm.name}> in provider <#{ems.try(:name)}>")
-  vm.stop
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Retirement
+          module StateMachines
+            class PreRetirement
+              def initialize(handle = $evm)
+                @handle = handle
+              end
+
+              def main
+                # Get vm from root object
+                vm = @handle.root['vm']
+                check_power_state(vm)
+              end
+
+              def check_power_state(vm)
+                unless vm.nil? || vm.attributes['power_state'] == 'off'
+                  ems = vm.ext_management_system
+                  @handle.log('info', "Powering Off VM <#{vm.name}> in provider <#{ems.try(:name)}>")
+                  vm.stop
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Infrastructure::VM::Retirement::StateMachines::PreRetirement.new.main
 end
