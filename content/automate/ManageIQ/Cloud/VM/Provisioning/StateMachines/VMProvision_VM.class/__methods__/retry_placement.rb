02234e549dda0492f5bfc33b350072e0513beb5e
Sam Lucidi <slucidi@redhat.com>
2017-02-13 00:30:38 -0500

    Parse error to determine whether to retry placement and count tries

--- content/automate/ManageIQ/Cloud/VM/Provisioning/StateMachines/VMProvision_VM.class/__methods__/retry_placement.rb
+++ content/automate/ManageIQ/Cloud/VM/Provisioning/StateMachines/VMProvision_VM.class/__methods__/retry_placement.rb
@@ -9,12 +9,25 @@ unless prov
   $evm.log(:error, "miq_provision object not provided")
   exit(MIQ_STOP)
 end
-status = $evm.inputs['status']
-$evm.root['ae_result'] = 'restart' 
-$evm.root['ae_next_state'] = 'Placement'
-$evm.log("info", "Provisioning #{prov.get_option(:vm_target_name)} failed, retrying placement.")
+
+DEFAULT_RETRIES = 3
+max_placement_retries = $evm.inputs['retries'] || DEFAULT_RETRIES
+if prov.message.include?("An error occurred while provisioning Instance")
+  retry_number = if $evm.state_var_exist?(:placement_retries)
+                   $evm.get_state_var(:placement_retries)
+                 else
+                   0
+                 end
+  if retry_number < max_placement_retries
+    $evm.root['ae_result'] = 'restart'
+    $evm.root['ae_next_state'] = 'Placement'
+    $evm.log("info", "Provisioning #{prov.get_option(:vm_target_name)} failed, retrying placement.")
+    $evm.set_state_var(:placement_retries, retry_number + 1)
+  end
+end
 
 # Update Status Message
+status = $evm.inputs['status']
 updated_message  = "[#{$evm.root['miq_server'].name}] "
 updated_message += "VM [#{prov.get_option(:vm_target_name)}] "
 updated_message += "Step [#{$evm.root['ae_state']}] "
