73d056b171b375c803eb5a6d06e13d460e087c57
william fitzgerald <wfitzger@redhat.com>
2017-04-12 14:41:10 -0400

    Refactor and create tests for 2 vm_retire_extend methods.
    Modified methods for infra and cloud vm methods to new style.
    Created new style test for each.
    
    https://www.pivotaltracker.com/story/show/143606515

--- content/automate/ManageIQ/Infrastructure/VM/Retirement/Email.class/__methods__/vm_retire_extend.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Retirement/Email.class/__methods__/vm_retire_extend.rb
@@ -3,83 +3,88 @@
 # VM has a retires_on value and is not already retired
 #
 
-# Number of days to automatically extend retirement
-vm_retire_extend_days = nil
-vm_retire_extend_days ||= $evm.object['vm_retire_extend_days']
-raise "ERROR - vm_retire_extend_days not found!" if vm_retire_extend_days.nil?
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Retirement
+          module Email
+            class VmRetireExtend
+              def initialize(handle = @evm)
+                @handle = handle
+              end
 
-$evm.log("info", "Number of days to extend: <#{vm_retire_extend_days}>")
+              def main
+                @handle.log("info", "Starting vm_retire_extend")
+                vm = @handle.root['vm']
+                check_retire_extend(vm)
+                @handle.log("info", "Ending vm_retire_extend")
+              end
 
-obj = $evm.object("process")
+              private
 
-vm = obj["vm"] || $evm.root["vm"]
-vm_name = vm.name
+              def check_retire_extend(vm)
+                vm_retire_extend_days = nil
+                vm_retire_extend_days ||= @handle.object['vm_retire_extend_days']
+                raise "ERROR - vm_retire_extend_days not found!" if vm_retire_extend_days.nil?
 
-# Bail out if VM does not have retirement date
-if vm.retires_on.nil? | vm.retires_on == ""
-  $evm.log("info", "VM '#{vm_name}' has no retirement date - extension bypassed")
-  exit MIQ_OK
-end
+                @handle.log("info", "Number of days to extend: <#{vm_retire_extend_days}>")
 
-# If VM is already retired to do not continue
-if vm.retired
-  $evm.log("info", "VM '#{vm_name}' is already retired. retires_on date: #{vm.retires_on}. No Action taken")
-end
+                vm_name = vm.name
 
-$evm.log("info", "VM: <#{vm_name}> current retirement date is #{vm.retires_on}")
+                if vm.retires_on.blank?
+                  @handle.log("info", "VM '#{vm_name}' has no retirement date - extension bypassed")
+                  exit MIQ_OK
+                end
 
-unless vm.retires_on.nil?
-  $evm.log("info", "Extending retirement <#{vm_retire_extend_days}> days for VM: <#{vm_name}>")
+                if vm.retired
+                  @handle.log("info", "VM '#{vm_name}' is already retired. retires_on date: #{vm.retires_on}. No Action taken")
+                  exit MIQ_OK
+                end
 
-  # Set new retirement date here
-  vm.extend_retires_on(vm_retire_extend_days, vm.retires_on)
+                @handle.log("info", "VM: <#{vm_name}> current retirement date is #{vm.retires_on}")
+                @handle.log("info", "Extending retirement <#{vm_retire_extend_days}> days for VM: <#{vm_name}>")
 
-  $evm.log("info", "VM: <#{vm_name}> new retirement date is #{vm.retires_on}")
+                vm.extend_retires_on(vm_retire_extend_days, vm.retires_on)
 
-  # Resetting last warning
-  # vm.retirement[:last_warn] = nil
+                @handle.log("info", "VM: <#{vm_name}> new retirement date is #{vm.retires_on}")
+                @handle.log("info", "Inspecting retirement vm: <#{vm.retirement_state.try}>")
 
-  $evm.log("info", "Inspecting retirement: <#{vm.retirement.inspect}>")
+                evm_owner_id = vm.attributes['evm_owner_id']
+                owner = nil
+                owner = @handle.vmdb('user', evm_owner_id) unless evm_owner_id.nil?
+                @handle.log("info", "Inspecting VM Owner: #{owner.inspect}")
 
-  ######################################
-  #
-  # VM Retirement Exended Email
-  #
-  ######################################
+                to = if owner
+                       owner.email
+                     else
+                       @handle.object['to_email_address']
+                     end
 
-  # Get VM Owner Name and Email
-  evm_owner_id = vm.attributes['evm_owner_id']
-  owner = nil
-  owner = $evm.vmdb('user', evm_owner_id) unless evm_owner_id.nil?
-  $evm.log("info", "Inspecting VM Owner: #{owner.inspect}")
+                from = nil
+                from ||= @handle.object['from_email_address']
 
-  # to_email_address from owner.email then from model if nil
-  if owner
-    to = owner.email
-  else
-    to = $evm.object['to_email_address']
+                signature = nil
+                signature ||= @handle.object['signature']
+
+                subject = "VM Retirement Extended for #{vm_name}"
+
+                body = "Hello, "
+                body += "<br><br>The retirement date for your virtual machine: [#{vm_name}] has been extended to: [#{vm.retires_on}]."
+                body += "<br><br> Thank you,"
+                body += "<br> #{signature}"
+
+                @handle.log("info", "Sending email to <#{to}> from <#{from}> subject: <#{subject}>")
+                @handle.execute('send_email', to, from, subject, body)
+              end
+            end
+          end
+        end
+      end
+    end
   end
+end
 
-  # Get from_email_address from model unless specified below
-  from = nil
-  from ||= $evm.object['from_email_address']
-
-  # Get signature from model unless specified below
-  signature = nil
-  signature ||= $evm.object['signature']
-
-  # email subject
-  subject = "VM Retirement Extended for #{vm_name}"
-
-  # Build email body
-  body = "Hello, "
-  body += "<br><br>The retirement date for your virtual machine: [#{vm_name}] has been extended to: [#{vm.retires_on}]."
-  body += "<br><br> Thank you,"
-  body += "<br> #{signature}"
-
-  #
-  # Send email
-  #
-  $evm.log("info", "Sending email to <#{to}> from <#{from}> subject: <#{subject}>")
-  $evm.execute('send_email', to, from, subject, body)
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Infrastructure::VM::Retirement::Email::VmRetireExtend.new.main
 end
