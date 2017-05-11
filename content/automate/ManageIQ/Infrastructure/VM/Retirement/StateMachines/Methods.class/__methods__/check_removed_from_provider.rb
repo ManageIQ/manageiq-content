fd319588ab1d2d91021ea60392f91dbce7163a53
william fitzgerald <wfitzger@redhat.com>
2017-05-11 16:05:44 -0400

    Refactor infra vm check_remove_from_provider method and test.
    Refactored the check_remove_from_provider method for Infrastructure Vm retirement.
    Updated tests.
    
    This PR is based on the issue below.
    ManageIQ/manageiq#12038
    
    @miq-bot add_label refactoring

--- content/automate/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/check_removed_from_provider.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/check_removed_from_provider.rb
@@ -2,14 +2,44 @@
 # Description: This method checks to see if the VM has been removed from the provider
 #
 
-vm = $evm.root['vm']
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Retirement
+          module StateMachines
+            class CheckRemovedFromProvider
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-$evm.root['ae_result'] = 'ok'
+              def main
+                check_removed_from_provider(vm)
+              end
 
-if vm && $evm.get_state_var('vm_removed_from_provider')
-  if vm.ext_management_system
-    vm.refresh
-    $evm.root['ae_result']     = 'retry'
-    $evm.root['ae_retry_interval'] = '60.seconds'
+              private
+
+              def vm
+                raise "ERROR - vm object not passed in" unless @handle.root['vm']
+                @handle.root['vm']
+              end
+
+              def check_removed_from_provider(vm)
+                @handle.root['ae_result'] = 'ok'
+                if vm.ext_management_system && @handle.get_state_var('vm_removed_from_provider')
+                  vm.refresh
+                  @handle.root['ae_result'] = 'retry'
+                  @handle.root['ae_retry_interval'] = '60.seconds'
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Infrastructure::VM::Retirement::StateMachines::CheckRemovedFromProvider.new.main
 end
