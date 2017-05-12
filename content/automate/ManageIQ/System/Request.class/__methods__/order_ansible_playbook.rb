bace533c620591b3206fe11f8a6be5bdbd7df080
Greg McCullough <gmccullo@redhat.com>
2017-05-12 13:10:10 -0400

    Merge pull request #113 from mkanoor/bz_1449361
    Order Ansible Playbook from a Custom Button using a Method
    (cherry picked from commit 3220db060e4f35e7908dbc66a1352a38dd0ffb4c)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1450485

---
+++ content/automate/ManageIQ/System/Request.class/__methods__/order_ansible_playbook.rb
@@ -0,0 +1,84 @@
+# Description: Launch an ansible playbook service
+# Required Parameters in the root object
+#   service_template_name
+# Optional Parameters in the root object
+# hosts   localhost|vm|ip1,ip2,ip3
+#   vmdb_object    If the hosts is vmdb_object, at runtime will use the ip address of the vmdb_object in the hosts field
+
+module ManageIQ
+  module Automate
+    module System
+      module Request
+        class OrderAnsiblePlaybook
+          ANSIBLE_DIALOG_VAR_REGEX = Regexp.new(/dialog_param_(.*)/)
+          def initialize(handle = $evm)
+            @handle = handle
+          end
+
+          def main
+            request = @handle.create_service_provision_request(service_template, extra_vars.merge(:hosts => hosts))
+            @handle.log(:info, "Submitted provision request #{request.id} for service template #{service_template_name}")
+          end
+
+          private
+
+          def extra_vars
+            key_list = @handle.root.attributes.keys.select { |k| k.start_with?('dialog_param') }
+            key_list.each_with_object({}) do |key, hash|
+              match_data = ANSIBLE_DIALOG_VAR_REGEX.match(key)
+              hash["param_#{match_data[1]}"] = @handle.root[key] if match_data
+            end
+          end
+
+          def hosts
+            if @handle.root['hosts'] == 'vmdb_object'
+              vmdb_object_ip
+            else
+              @handle.root['hosts'] || @handle.root['dialog_hosts']
+            end
+          end
+
+          def vmdb_object_ip
+            vmdb_object.try(:ipaddresses).try(:first).tap do |ip|
+              if ip.nil?
+                raise "IP address not specified for vmdb_object"
+              end
+            end
+          end
+
+          def vmdb_object
+            if @handle.root['vmdb_object_type'].nil?
+              raise "vmdb_object_type missing in root object"
+            end
+
+            vmdb_object_type = @handle.root['vmdb_object_type']
+
+            if @handle.root[vmdb_object_type].nil?
+              raise "vmdb_object #{vmdb_object_type} missing in root object"
+            end
+            @handle.root[vmdb_object_type]
+          end
+
+          def service_template
+            @service_template ||= @handle.vmdb('ServiceTemplate').where(:name => service_template_name).first.tap do |st|
+              if st.nil?
+                raise "Service Template #{@handle.root['service_template_name']} not found"
+              end
+            end
+          end
+
+          def service_template_name
+            if @handle.root['service_template_name'].nil?
+              raise "service_template_name is a required parameter"
+            end
+            @handle.root['service_template_name']
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::System::Request::OrderAnsiblePlaybook.new.main
+end
---
+++ spec/content/automate/ManageIQ/System/Request.class/__methods__/order_ansible_playbook_spec.rb
@@ -0,0 +1,128 @@
+require_domain_file
+
+describe ManageIQ::Automate::System::Request::OrderAnsiblePlaybook do
+  let(:root_object) do
+    Spec::Support::MiqAeMockObject.new(attributes)
+  end
+
+  let(:ae_service) do
+    Spec::Support::MiqAeMockService.new(root_object).tap do |service|
+      current_object = Spec::Support::MiqAeMockObject.new
+      current_object.parent = root_object
+      service.object = current_object
+    end
+  end
+
+  let(:ip1) { "1.1.1.94" }
+
+  let(:vm) { FactoryGirl.create(:vm_vmware, :name => 'fred') }
+  let(:svc_vm) { MiqAeMethodService::MiqAeServiceVm.find(vm.id) }
+
+  let(:svc_template) { FactoryGirl.create(:service_template_ansible_playbook, :name => 'fred') }
+  let(:svc_service_template) do
+    MiqAeMethodService::MiqAeServiceServiceTemplate.find(svc_template.id)
+  end
+
+  let(:miq_request) { FactoryGirl.create(:service_template_provision_request) }
+  let(:svc_miq_request) do
+    MiqAeMethodService::MiqAeServiceMiqRequest.find(miq_request.id)
+  end
+
+  let(:svc_vmdb_handle) { MiqAeMethodService::MiqAeServiceServiceTemplate }
+
+  shared_examples_for "order playbook" do
+    it "creates request" do
+      allow(ae_service).to receive(:vmdb).with('ServiceTemplate').and_return(svc_vmdb_handle)
+      allow(svc_vmdb_handle).to receive(:where).with(:name => svc_template.name).and_return([svc_service_template])
+      allow(svc_vm).to receive(:ipaddresses).and_return([ip1])
+      expect(ae_service).to receive(:create_service_provision_request).with(svc_service_template, extra_vars).and_return(svc_miq_request)
+
+      described_class.new(ae_service).main
+    end
+  end
+
+  context "with no host" do
+    let(:extra_vars) do
+      { :hosts       => nil,
+        'param_var1' => 'A',
+        'param_var2' => 'B' }
+    end
+    let(:attributes) do
+      { 'service_template_name' => svc_service_template.name,
+        'dialog_param_var1'     => 'A',
+        'dialog_param_var2'     => 'B',
+        'vmdb_object_type'      => 'vm',
+        'vm'                    => svc_vm }
+    end
+
+    it_behaves_like "order playbook"
+
+    it "order playbook with incorrect service_template_name should raise error" do
+      allow(ae_service).to receive(:vmdb).with('ServiceTemplate').and_return(svc_vmdb_handle)
+      allow(svc_vmdb_handle).to receive(:where).with(:name => svc_template.name).and_return([nil])
+
+      expect { described_class.new(ae_service).main }.to raise_error(/not found/)
+    end
+  end
+
+  context "with no service_template_name" do
+    let(:attributes) { {} }
+    it "should raise error" do
+      expect { described_class.new(ae_service).main }.to raise_error(/service_template_name/)
+    end
+  end
+
+  context "with vm host" do
+    let(:attributes) do
+      { 'service_template_name' => svc_service_template.name,
+        'dialog_param_var1'     => 'A',
+        'dialog_param_var2'     => 'B',
+        'hosts'                 => 'vmdb_object',
+        'vmdb_object_type'      => 'vm',
+        'vm'                    => svc_vm }
+    end
+
+    let(:extra_vars) do
+      { :hosts       => ip1,
+        'param_var1' => 'A',
+        'param_var2' => 'B' }
+    end
+
+    it_behaves_like "order playbook"
+  end
+
+  context "with vm host but no vm" do
+    let(:attributes) do
+      { 'service_template_name' => svc_service_template.name,
+        'dialog_param_var1'     => 'A',
+        'dialog_param_var2'     => 'B',
+        'hosts'                 => 'vmdb_object' }
+    end
+
+    it "raises an error" do
+      allow(ae_service).to receive(:vmdb).with('ServiceTemplate').and_return(svc_vmdb_handle)
+      allow(svc_vmdb_handle).to receive(:where).with(:name => svc_template.name).and_return([svc_service_template])
+
+      expect { described_class.new(ae_service).main }.to raise_error(/vmdb_object_type missing in root object/)
+    end
+  end
+
+  context "with vm host but no ip address" do
+    let(:attributes) do
+      { 'service_template_name' => svc_service_template.name,
+        'dialog_param_var1'     => 'A',
+        'dialog_param_var2'     => 'B',
+        'vm'                    => svc_vm,
+        'vmdb_object_type'      => 'vm',
+        'hosts'                 => 'vmdb_object' }
+    end
+
+    it "raises an error" do
+      allow(ae_service).to receive(:vmdb).with('ServiceTemplate').and_return(svc_vmdb_handle)
+      allow(svc_vmdb_handle).to receive(:where).with(:name => svc_template.name).and_return([svc_service_template])
+      allow(svc_vm).to receive(:ipaddresses).and_return([nil])
+
+      expect { described_class.new(ae_service).main }.to raise_error(/IP address not specified for vm/)
+    end
+  end
+end
