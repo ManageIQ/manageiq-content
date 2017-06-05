670428321e60f7c40ba9b643a615187a7fce2474
Greg McCullough <gmccullo@redhat.com>
2017-06-05 08:54:07 -0400

    Merge pull request #121 from matobet/v2v-driver-automate
    v2v: Add support for driver ISOs
    (cherry picked from commit ec6ee1fc5a81635aa41556e4d13b2f9e54cd2ff1)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1459996

---
+++ content/automate/ManageIQ/Infrastructure/VM/Transform/Import.class/__methods__/list_driver_isos.rb
@@ -0,0 +1,45 @@
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Transform
+          module Import
+            class ListDriverIsos
+              def initialize(handle = $evm)
+                @handle = handle
+              end
+
+              def main
+                values_hash = {}
+                values_hash[nil] = '-- select provider first --'
+
+                provider_id = @handle.root['dialog_provider']
+                if provider_id.present? && provider_id != '!'
+                  provider = @handle.vmdb(:ext_management_system, provider_id)
+                  unless provider.nil?
+                    values_hash[nil] = '-- select drivers ISO from list --'
+                    provider.iso_datastore.iso_images.pluck(:name).grep(/toolsSetup/).each do |iso|
+                      values_hash[iso] = iso
+                    end
+                  end
+                end
+                list_values = {
+                  'sort_by'   => :description,
+                  'data_type' => :string,
+                  'required'  => true,
+                  'values'    => values_hash,
+                  'visible'   => @handle.root['dialog_install_drivers']
+                }
+                list_values.each { |key, value| @handle.object[key] = value }
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Infrastructure::VM::Transform::Import::ListDriverIsos.new.main
+end
