49405d31639b44df559900022640e0d289be4ec6
Brant Evans <bevans@redhat.com>
2017-06-15 14:16:31 -0700

    check for ems_id instead of both archived and orphaned

--- content/automate/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/used.rb
+++ content/automate/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/used.rb
@@ -6,7 +6,7 @@ def consumption(source)
   {
     :cpu                 => source.allocated_vcpu,
     :memory              => source.allocated_memory,
-    :vms                 => source.vms.count { |vm| vm.id unless vm.archived || vm.orphaned },
+    :vms                 => source.vms.count { |vm| vm.id if vm.ems_id },
     :storage             => source.allocated_storage,
     :provisioned_storage => source.provisioned_storage
   }
