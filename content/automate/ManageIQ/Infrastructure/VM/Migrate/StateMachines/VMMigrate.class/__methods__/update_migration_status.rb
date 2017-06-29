b8bfb5d7602baba2ae2aa11fba5a71ef180b29e6
Madhu Kanoor <mkanoor@redhat.com>
2017-06-29 16:50:13 -0400

    Merge pull request #125 from billfitzgerald0120/vm_migrate
    Enhanced messaging and error notification for update_migration_status method.
    (cherry picked from commit 7a789402519c390e8b21c64f0fe5566f4987ff54)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1478520

--- content/automate/ManageIQ/Infrastructure/VM/Migrate/StateMachines/VMMigrate.class/__methods__/update_migration_status.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Migrate/StateMachines/VMMigrate.class/__methods__/update_migration_status.rb
@@ -1,18 +1,62 @@
 #
 # Description: This method updates the migration status
 #
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Migrate
+          module StateMachines
+            class UpdateMigrationStatus
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-miq_task = $evm.root['vm_migrate_task']
-status   = $evm.inputs['status']
+              def main
+                update_migration_status(task)
+              end
 
-# Update Status for on_entry,on_exit
-if $evm.root['ae_result'] == 'ok'
-  if status == 'migration_complete'
-    message = 'VM Migrated Successfully'
-    miq_task.finished(message)
+              private
+
+              def task
+                raise "ERROR - task object not passed in" unless @handle.root['vm_migrate_task']
+                @handle.root['vm_migrate_task']
+              end
+
+              def update_migration_status(task)
+                status = @handle.inputs['status']
+
+                # Update Status Message
+                updated_message  = "Server [#{@handle.root['miq_server'].name}] "
+                updated_message += "VM [#{task.source.name}] "
+                updated_message += "Step [#{@handle.root['ae_state']}] "
+                updated_message += "Status [#{status}] "
+                updated_message += "Message [#{task.message}] "
+                updated_message += "Current Retry Number [#{@handle.root['ae_state_retries']}]" if @handle.root['ae_result'] == 'retry'
+                task.miq_request.user_message = updated_message
+                task.message = status
+
+                if @handle.root['ae_result'] == "error"
+                  @handle.create_notification(:level   => "error", \
+                                              :message => "VM Migration Error: #{updated_message}")
+                  @handle.log(:error, "VM Migration Error: #{updated_message}")
+                end
+
+                # Update Status for on_entry,on_exit
+                if @handle.root['ae_result'] == 'ok'
+                  if status == 'migration_complete'
+                    task.finished('VM Migrated Successfully')
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
-  miq_task.message = status
 end
 
-# Update Status for on_error
-miq_task.message = status if $evm.root['ae_result'] == 'error'
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Infrastructure::VM::Migrate::StateMachines::UpdateMigrationStatus.new.main
+end
