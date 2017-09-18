2f66e51c61b866a28865d208f12b82b3d76a5814
Greg McCullough <gmccullo@redhat.com>
2017-09-18 13:51:39 -0400

    Merge pull request #186 from tinaafitz/revert_pr148
    Revert "Support TTL (Time To Live) value for services."
    (cherry picked from commit 546e1a4ea1919f8bb92020cf136ed087ae67f741)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1479407

--- spec/content/automate/ManageIQ/Service/Generic/StateMachines/GenericLifecycle.class/__methods__/start_spec.rb
+++ spec/content/automate/ManageIQ/Service/Generic/StateMachines/GenericLifecycle.class/__methods__/start_spec.rb
@@ -1,106 +1,23 @@
 require_domain_file
 
 describe ManageIQ::Automate::Service::Generic::StateMachines::GenericLifecycle::Start do
-  let(:request) { FactoryGirl.create(:service_template_provision_request, :requester => FactoryGirl.create(:user_admin)) }
-  let(:job_template) { FactoryGirl.create(:ansible_configuration_script, :manager => FactoryGirl.create(:automation_manager)) }
-  let(:service_ansible_tower) { FactoryGirl.create(:service_ansible_tower, :job_template => job_template, :options => config_info_options) }
+  let(:admin) { FactoryGirl.create(:user_admin) }
+  let(:request) { FactoryGirl.create(:service_template_provision_request, :requester => admin) }
+  let(:ansible_tower_manager) { FactoryGirl.create(:automation_manager) }
+  let(:job_template) { FactoryGirl.create(:ansible_configuration_script, :manager => ansible_tower_manager) }
+  let(:service_ansible_tower) { FactoryGirl.create(:service_ansible_tower, :job_template => job_template) }
+  let(:task) { FactoryGirl.create(:service_template_provision_task, :destination => service_ansible_tower, :miq_request => request) }
+  let(:svc_task) { MiqAeMethodService::MiqAeServiceServiceTemplateProvisionTask.find(task.id) }
   let(:svc_service) { MiqAeMethodService::MiqAeServiceServiceAnsibleTower.find(service_ansible_tower.id) }
-  let(:root_object) do
-    Spec::Support::MiqAeMockObject.new('service' => svc_service, 'service_action' => 'Provision',
-                  'ae_state_max_retries' => 100)
-  end
+  let(:root_object) { Spec::Support::MiqAeMockObject.new('service' => svc_service, 'service_action' => 'Provision') }
   let(:ae_service) { Spec::Support::MiqAeMockService.new(root_object) }
-  let(:config_info_options) do
-    {
-      :config_info => {
-        :provision  => {
-          :execution_ttl => ttl
-        },
-        :retirement => {
-          :execution_ttl => ttl
-        }
-      }
-    }
-  end
 
-  shared_examples_for "#ttl" do
-    it "check" do
+  context "Start" do
+    it "creates notification " do
       allow(ae_service).to receive(:create_notification)
 
       described_class.new(ae_service).main
-      expect(ae_service.root['ae_retry_interval']).to eq(ae_retry_interval)
+      expect(ae_service.root['ae_result']).to eq("ok")
     end
-  end
-
-  context "Start 7700 ttl, 100 retries eq interval 77" do
-    let(:ae_retry_interval) { 77 }
-    let(:ttl) { 7700 }
-    it_behaves_like "#ttl"
-  end
-
-  context "Start 60000 ttl, 100 retries eq interval 600" do
-    let(:ae_retry_interval) { 600 }
-    let(:ttl) { 60_000 }
-    it_behaves_like "#ttl"
-  end
-
-  context "Start 600 ttl, 100 retries interval nil" do
-    let(:ae_retry_interval) { nil }
-    let(:ttl) { 600 }
-    it_behaves_like "#ttl"
-  end
-
-  context "Start 0 ttl, 100 retries, interval nil" do
-    let(:ae_retry_interval) { nil }
-    let(:ttl) { 0 }
-    it_behaves_like "#ttl"
-  end
-
-  context "Start retirement tests, " do
-    let(:root_object) do
-      Spec::Support::MiqAeMockObject.new('service' => svc_service, 'service_action' => 'Retirement', 'ae_state_max_retries' => 100)
-    end
-
-    context " 7700 ttl, 100 retries eq interval 77" do
-      let(:ae_retry_interval) { 77 }
-      let(:ttl) { 7700 }
-      it_behaves_like "#ttl"
-    end
-
-    context " 60000 ttl, 100 retries eq interval 600" do
-      let(:ae_retry_interval) { 600 }
-      let(:ttl) { 60_000 }
-      it_behaves_like "#ttl"
-    end
-
-    context " 600 ttl, 100 retries eq interval nil" do
-      let(:ae_retry_interval) { nil }
-      let(:ttl) { 600 }
-      it_behaves_like "#ttl"
-    end
-
-    context " 0 ttl, 100 retries, interval nil" do
-      let(:ae_retry_interval) { nil }
-      let(:ttl) { 0 }
-      it_behaves_like "#ttl"
-    end
-  end
-
-  context "Start 6000 ttl, 50 retries eq interval 120" do
-    let(:ae_retry_interval) { 120 }
-    let(:ttl) { 6000 }
-    let(:root_object) do
-      Spec::Support::MiqAeMockObject.new('service' => svc_service, 'service_action' => 'Provision', 'ae_state_max_retries' => 50)
-    end
-    it_behaves_like "#ttl"
-  end
-
-  context "Start 6000 ttl, 0 retries eq interval nil" do
-    let(:ae_retry_interval) { nil }
-    let(:ttl) { 6000 }
-    let(:root_object) do
-      Spec::Support::MiqAeMockObject.new('service' => svc_service, 'service_action' => 'Provision', 'ae_state_max_retries' => 0)
-    end
-    it_behaves_like "#ttl"
   end
 end
