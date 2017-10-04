9cd896a55659b82c8862db7bffa166846244ecfb
william fitzgerald <wfitzger@redhat.com>
2017-10-04 14:18:46 -0400

    Refactor used quota method and test
    This PR is based on on the issue below.
    https://github.com/ManageIQ/manageiq/issues/12038

--- content/automate/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/used.rb
+++ content/automate/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/used.rb
@@ -2,14 +2,47 @@
 # Description: calculate entity used quota values
 #
 
-def consumption(source)
-  {
-    :cpu                 => source.allocated_vcpu,
-    :memory              => source.allocated_memory,
-    :vms                 => source.vms.count { |vm| vm.id if vm.ems_id },
-    :storage             => source.allocated_storage,
-    :provisioned_storage => source.provisioned_storage
-  }
+module ManageIQ
+  module Automate
+    module System
+      module CommonMethods
+        module QuotaMethods
+          class Used
+            def initialize(handle = $evm)
+              @handle = handle
+            end
+
+            def main
+              used(quota_source)
+            end
+
+            private
+
+            def used(quota_source)
+              @handle.root['quota_used'] = consumption(quota_source)
+            end
+
+            def quota_source
+              raise "ERROR - quota_source not found" unless @handle.root['quota_source']
+              @handle.root['quota_source']
+            end
+
+            def consumption(source)
+              {
+                :cpu                 => source.allocated_vcpu,
+                :memory              => source.allocated_memory,
+                :vms                 => source.vms.count { |vm| vm.id if vm.ems_id },
+                :storage             => source.allocated_storage,
+                :provisioned_storage => source.provisioned_storage
+              }
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
-$evm.root['quota_used'] = consumption($evm.root['quota_source']) if $evm.root['quota_source']
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::System::CommonMethods::QuotaMethods::Used.new.main
+end
