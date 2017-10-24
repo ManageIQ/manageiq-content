ee53f815217c3b20e157c7601c36cb57c1bf2c43
Shmuel Melamud <smelamud@redhat.com>
2017-10-24 17:20:02 +0300

    v2v: Extend 'VM Transform' dialog to select VMs by tag
    Added tag category and tag name fields to the 'VM Transform' dialog to
    be able to select VMs for V2V by tag. Hide these fields if the dialog is
    open from VM details view. Hide VM name input field if the dialog is
    open from VM list view. Start several V2V requests if several VMs are
    selected by tag.

--- content/automate/ManageIQ/Infrastructure/VM/Transform/Import.class/__methods__/create_vm_import_request.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Transform/Import.class/__methods__/create_vm_import_request.rb
@@ -14,27 +14,16 @@ module ManageIQ
               end
 
               def main
-                validate_root_args %w(vm dialog_name dialog_provider dialog_cluster dialog_storage dialog_sparse)
-
-                options = {
-                  :namespace     => 'Infrastructure/VM/Transform/StateMachines',
-                  :class_name    => 'VmImport',
-                  :instance_name => 'default',
-                  :message       => 'create',
-                  :attrs         => {
-                    'Vm::vm'      => @handle.root['vm'].id,
-                    'name'        => @handle.root['dialog_name'],
-                    'provider_id' => @handle.root['dialog_provider'],
-                    'cluster_id'  => @handle.root['dialog_cluster'],
-                    'storage_id'  => @handle.root['dialog_storage'],
-                    'sparse'      => @handle.root['dialog_sparse'],
-                    'drivers_iso' => @handle.root['dialog_install_drivers'] && @handle.root['dialog_drivers_iso']
-                  },
-                  :user_id       => @handle.root['user'].id
-                }
-
-                auto_approve = true
-                @handle.execute('create_automation_request', options, @handle.root['user'].userid, auto_approve)
+                validate_root_args %w(vm dialog_provider dialog_cluster dialog_storage dialog_sparse)
+                if not @handle.root['dialog_tag_category'].empty? and not @handle.root['dialog_tag_name'].empty?
+                  tag = "/#{@handle.root['dialog_tag_category']}/#{@handle.root['dialog_tag_name']}"
+                  tagged_vms = @handle.vmdb(:vm).find_tagged_with(:all => tag, :ns => '/managed')
+                  tagged_vms.each do |vm|
+                    create_request(vm, '')
+                  end
+                else
+                  create_request(@handle.root['vm'], @handle.root['dialog_name'])
+                end
               end
 
               def validate_root_args(arg_names)
@@ -44,6 +33,28 @@ module ManageIQ
                   @handle.log(:error, msg)
                   raise msg
                 end
+              end
+
+              def create_request(vm, target_name)
+                options = {
+                    :namespace     => 'Infrastructure/VM/Transform/StateMachines',
+                    :class_name    => 'VmImport',
+                    :instance_name => 'default',
+                    :message       => 'create',
+                    :attrs         => {
+                        'Vm::vm'      => vm.id,
+                        'name'        => target_name.empty? ? vm.name : target_name,
+                        'provider_id' => @handle.root['dialog_provider'],
+                        'cluster_id'  => @handle.root['dialog_cluster'],
+                        'storage_id'  => @handle.root['dialog_storage'],
+                        'sparse'      => @handle.root['dialog_sparse'],
+                        'drivers_iso' => @handle.root['dialog_install_drivers'] && @handle.root['dialog_drivers_iso']
+                    },
+                    :user_id       => @handle.root['user'].id
+                }
+
+                auto_approve = true
+                @handle.execute('create_automation_request', options, @handle.root['user'].userid, auto_approve)
               end
             end
           end
