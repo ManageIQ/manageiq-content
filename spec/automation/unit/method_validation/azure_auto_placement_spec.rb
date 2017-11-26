bffa9847485196aed4e021788f8dc3191ca0a642
Greg McCullough <gmccullo@redhat.com>
2017-11-25 21:42:15 -0500

    ResourceGroup model is now STI and tests need to use Azure sub-class
    Based on https://github.com/ManageIQ/manageiq-providers-azure/pull/165

--- spec/automation/unit/method_validation/azure_auto_placement_spec.rb
+++ spec/automation/unit/method_validation/azure_auto_placement_spec.rb
@@ -15,7 +15,7 @@ describe "azure best fit" do
      :placement_auto => [true, 1],
      :instance_type  => [m2_small_flavor.id, m2_small_flavor.name]}
   end
-  let(:resource_group)    { FactoryGirl.create(:resource_group, :ems_id => ems.id) }
+  let(:resource_group)    { FactoryGirl.create(:azure_resource_group, :ems_id => ems.id) }
   let(:user)              { FactoryGirl.create(:user_with_group) }
   let(:vm_template)       { FactoryGirl.create(:template_azure, :ext_management_system => ems) }
   let(:ws) do
