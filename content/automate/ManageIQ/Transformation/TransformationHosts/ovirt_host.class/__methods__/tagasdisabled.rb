c2737ac73636ad3957ebec9a49e1374b0c421a37
Fabien Dupont <fdupont@redhat.com>
2018-02-14 13:26:42 -0500

    Removed code used for actual transformation, focusing this PR on Transformation Host role.

--- content/automate/ManageIQ/Transformation/TransformationHosts/ovirt_host.class/__methods__/tagasdisabled.rb
+++ content/automate/ManageIQ/Transformation/TransformationHosts/ovirt_host.class/__methods__/tagasdisabled.rb
@@ -1,24 +1,26 @@
 #
-# 
+#
 #
 
-module MigrationFactory
-  module ConversionHost
-    class TagAsDisabled
-      def initialize(handle = $evm)
-        @debug = true
-        @handle = handle
-      end
-      
-      def main
-        host = @handle.root['host']
-        raise "No host found. Aborting." if host.nil?
-        host.tag_assign('mf_conversion_host/false')
+module Transformation
+  module TransformationHosts
+    module OVirtHost
+      class TagAsDisabled
+        def initialize(handle = $evm)
+          @handle = handle
+        end
+
+        def main
+          host = @handle.root['host']
+          raise "No host found. Aborting." if host.nil?
+          host.tag_assign('v2v_transformation_host/false')
+          host.tag_unassign('v2v_transformation_method/vddk')
+        end
       end
     end
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  MigrationFactory::ConversionHost::TagAsDisabled.new.main
+  Transformation::TransformationHosts:OVirtHost::TagAsDisabled.new.main
 end
