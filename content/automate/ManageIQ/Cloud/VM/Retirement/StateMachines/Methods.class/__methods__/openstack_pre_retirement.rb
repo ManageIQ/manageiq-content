0b11ec8677777eb69d6b5941d078d8df4ea0b3c0
Patrik Kománek <pkomanek@redhat.com>
2018-03-02 11:46:56 +0100

    Method refactoring.

--- content/automate/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/openstack_pre_retirement.rb
+++ content/automate/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/openstack_pre_retirement.rb
@@ -1,10 +1,37 @@
 #
 # Description: This method suspends the Openstack Instance
 #
+module ManageIQ
+  module Automate
+    module Cloud
+      module VM
+        module Retirement
+          module StateMachines
+            module Methods
+              class OpenstackPreRetirement
+                def initialize(handle = $evm)
+                  @handle = handle
+                end
 
-vm = $evm.root['vm']
-unless vm.nil? || vm.attributes['power_state'] == 'off'
-  ems = vm.ext_management_system
-  $evm.log('info', "Suspending Openstack Instance <#{vm.name}> in EMS <#{ems.try(:name)}")
-  vm.suspend if ems
+                def main
+                  vm = @handle.root['vm']
+                  unless vm.nil? || vm.attributes['power_state'] == 'off'
+                    ems = vm.ext_management_system
+                    if ems
+                      @handle.log('info', "Suspending Openstack Instance <#{vm.name}> in EMS <#{ems.try(:name)}")
+                      vm.suspend
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Cloud::VM::Retirement::StateMachines::Methods::OpenstackPreRetirement.new.main
 end
