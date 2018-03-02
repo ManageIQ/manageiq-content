04ce09a4729c98bef057eb158eec5a32c6fa6a38
Patrik Kománek <pkomanek@redhat.com>
2018-03-02 11:36:11 +0100

    Method refactoring.

--- content/automate/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/pre_retirement.rb
+++ content/automate/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/pre_retirement.rb
@@ -1,10 +1,37 @@
 #
 # Description: This method stops a cloud Instance
 #
+module ManageIQ
+  module Automate
+    module Cloud
+      module VM
+        module Retirement
+          module StateMachines
+            module Methods
+              class PreRetirement
+                def initialize(handle = $evm)
+                  @handle = handle
+                end
 
-vm = $evm.root['vm']
-if vm && vm.power_state == 'on'
-  ems = vm.ext_management_system
-  $evm.log('info', "Stopping Instance <#{vm.name}> in EMS <#{ems.try(:name)}>")
-  vm.stop if ems
+                def main
+                  vm = @handle.root['vm']
+                  if vm && vm.power_state == 'on'
+                    ems = vm.ext_management_system
+                    if ems
+                      @handle.log('info', "Stopping Instance <#{vm.name}> in EMS <#{ems.name}>")
+                      vm.stop
+                    end
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Cloud::VM::Retirement::StateMachines::Methods::PreRetirement.new.main
 end
