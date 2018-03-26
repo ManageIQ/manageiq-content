826b7f4c93d57417aa4b6504469504e5b9e17fa6
d-m-u <drewuhlmann@gmail.com>
2018-03-26 14:28:01 -0400

    Add namespaced tests for service retirement with task

---
+++ spec/content/automate/ManageIQ/Service/Retirement/StateMachines/ServiceRetirement.class/__methods__/update_service_retirement_status_spec.rb
@@ -0,0 +1,64 @@
+require_domain_file
+
+describe ManageIQ::Automate::Service::Retirement::StateMachines::
+ServiceRetirement::UpdateServiceRetirementStatus do
+  let(:user)                  { FactoryGirl.create(:user_with_group) }
+  let(:miq_server)            { EvmSpecHelper.local_miq_server }
+  let(:miq_request_task) do
+    FactoryGirl.create(:miq_request_task,
+                       :miq_request => request, :state => 'fred')
+  end
+
+  let(:request) do
+    FactoryGirl.create(:service_retire_request, :requester => user)
+  end
+
+  let(:ae_service) do
+    Spec::Support::MiqAeMockService.new(root_object).tap do |service|
+      current_object = Spec::Support::MiqAeMockObject.new
+      current_object.parent = root_object
+      service.object = current_object
+    end
+  end
+
+  let(:svc_model_miq_server)       { MiqAeMethodService::MiqAeServiceMiqServer.find(miq_server.id) }
+  let(:svc_model_miq_request_task) { MiqAeMethodService::MiqAeServiceMiqRequestTask.find(miq_request_task.id) }
+  let(:svc_model_request) do
+    MiqAeMethodService::MiqAeServiceServiceRetireRequest.find(request.id)
+  end
+
+  context "with a stp request object" do
+    let(:root_hash) do
+      { }
+    end
+
+    let(:root_object) do
+      obj = Spec::Support::MiqAeMockObject.new(root_hash)
+      obj["service_retire_task"] = svc_model_miq_request_task
+      obj["miq_server"] = svc_model_miq_server
+      obj
+    end
+
+    before do
+      allow(ae_service).to receive(:inputs) { {'status' => "fred"} }
+      ae_service.root['ae_result'] = 'ok'
+    end
+
+    it "method succeeds" do
+      described_class.new(ae_service).main
+      expect(svc_model_request.reload.status).to eq('Ok')
+    end
+
+    it "request message set properly" do
+      described_class.new(ae_service).main
+      msg = "Server [#{miq_server.name}] Step [] Status [fred] Message [] "
+      expect(svc_model_request.reload.message).to eq(msg)
+    end
+
+    it "creates notification due to ae_result is 'error'" do
+      ae_service.root['ae_result'] = "error"
+      expect(ae_service).to receive(:create_notification)
+      described_class.new(ae_service).main
+    end
+  end
+end
