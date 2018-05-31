35ca01c522188967b71d0d9877a9db9ba40c7f5b
Greg McCullough <gmccullo@redhat.com>
2018-05-31 16:08:42 -0400

    Merge pull request #301 from fdupont-redhat/v2v_gaprindashvili-3
    Add V2V for VMware to oVirt / RHV
    (cherry picked from commit dc72f1fed8e5faac9e4dca0ad3c6a4982dc5d49f)

---
+++ content/automate/ManageIQ/Transformation/Common.class/__methods__/acquiretransformationhost.rb
@@ -0,0 +1,47 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Common
+        class AcquireTransformationHost
+          def initialize(handle = $evm)
+            @handle = handle
+          end
+
+          def main
+            factory_config = @handle.get_state_var(:factory_config)
+            raise "No factory config found. Aborting." if factory_config.nil?
+
+            task = @handle.root['service_template_transformation_plan_task']
+            @handle.log(:info, "Task: #{task.inspect}")
+            source_vm = task.source
+            @handle.log(:info, "Source VM: #{source_vm.name}")
+            source_cluster = source_vm.ems_cluster
+            @handle.log(:info, "Source Cluster: #{source_cluster.name}")
+            @handle.log(:info, "Destination Cluster: #{task.transformation_destination(source_cluster).name}")
+            destination_ems = task.transformation_destination(source_cluster).ext_management_system
+            raise "Invalid destination EMS type: #{destination_ems.emstype}. Aborting." unless destination_ems.emstype == "rhevm"
+
+            transformation_host = ManageIQ::Automate::Transformation::TransformationHosts::Common::Utils.get_transformation_host(destination_ems, @handle.get_state_var(:transformation_method), factory_config)
+            if transformation_host.nil?
+              @handle.log(:info, "No transformation host available. Retrying.")
+              @handle.root['ae_result'] = 'retry'
+              @handle.root['ae_retry_server_affinity'] = true
+              @handle.root['ae_retry_interval'] = $evm.object['check_convert_interval'] || '1.minutes'
+            else
+              @handle.log(:info, "Transformation Host: #{transformation_host.name}.")
+              task.set_option(:transformation_host_id, transformation_host.id)
+              task.set_option(:transformation_host_name, transformation_host.name)
+            end
+          rescue => e
+            @handle.set_state_var(:ae_state_progress, 'message' => e.message)
+            raise
+          end
+        end
+      end
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Transformation::Common::AcquireTransformationHost.new.main
+end
---
+++ content/automate/ManageIQ/Transformation/Infrastructure/VM/vmwarews.class/__methods__/utils.rb
@@ -0,0 +1,66 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Infrastructure
+        module VM
+          module VMware
+            class Utils
+              require 'rbvmomi'
+
+              def initialize(handle = $evm)
+                @handle = handle
+              end
+
+              def main
+              end
+
+              def connect_to_provider(ems)
+                ipaddress = ems.ipaddress
+                ipaddress ||= ems.hostname
+                RbVmomi::VIM.connect(:host => ipaddress, :user => ems.authentication_userid, :password => ems.authentication_password, :insecure => true)
+              end
+
+              def vm_get_ref(vim, vm)
+                dc = vim.serviceInstance.find_datacenter(vm.v_owning_datacenter)
+                raise "Datacenter '#{vm.datacenter.name}' not found in vCenter" unless dc
+                vim.serviceInstance.content.searchIndex.FindByUuid(:datacenter => dc, :uuid => vm.uid_ems, :vmSearch => true, :instanceUuid => false)
+              end
+
+              def self.get_vcenter_fingerprint(ems, handle = $evm)
+                command = "openssl s_client -connect #{ems.hostname}:443 2>\/dev\/null | openssl x509 -noout -fingerprint -sha1"
+                ssl_fingerprint = `#{command}`
+                fingerprint = ssl_fingerprint[17..ssl_fingerprint.size - 2]
+                handle.log(:info, "vCenter fingerprint: #{fingerprint}")
+                fingerprint
+              end
+
+              def self.vm_rename(vm, new_name, handle = $evm)
+                ems = vm.ext_management_system
+                ems_endpoint = ems.ipaddress || ems.hostname
+                vim = RbVmomi::VIM.connect(:host => ems_endpoint, :user => ems.authentication_userid, :password => ems.authentication_password, :insecure => true)
+
+                sleep 2
+                dc = vim.serviceInstance.find_datacenter(vm.v_owning_datacenter)
+                raise "Datacenter '#{vm.v_owning_datacenter}' not found in vCenter" unless dc
+
+                sleep 2
+                vim_vm = dc.find_vm(vm.name)
+                raise "Unable to locate #{vm.name} in data center #{dc}" unless vim_vm
+                begin
+                  sleep 2
+                  vim_vm.ReconfigVM_Task(:spec => RbVmomi::VIM::VirtualMachineConfigSpec(:name => new_name)).wait_for_completion
+                  sleep 2
+                  dc.find_vm(new_name)
+                  handle.log(:info, "Successfully renamed #{options[:source_vm]} to #{options[:new_name]}.")
+                rescue
+                  return false
+                end
+                true
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
