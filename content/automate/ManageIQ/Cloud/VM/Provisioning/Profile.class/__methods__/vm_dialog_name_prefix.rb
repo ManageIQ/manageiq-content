d4552b7b1aaacfb8481a83c235dfacb15d660c1c
Patrik Kománek <pkomanek@redhat.com>
2018-07-10 15:51:53 +0200

    vm_dialog_name_prefix method refactoring and adding a new spec.

--- content/automate/ManageIQ/Cloud/VM/Provisioning/Profile.class/__methods__/vm_dialog_name_prefix.rb
+++ content/automate/ManageIQ/Cloud/VM/Provisioning/Profile.class/__methods__/vm_dialog_name_prefix.rb
@@ -1,20 +1,40 @@
 #
 # Description: This is the default method to determine the dialog prefix name to use
 #
+module ManageIQ
+  module Automate
+    module Cloud
+      module VM
+        module Provisioning
+          module Profile
+            class VmDialogNamePrefix
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-platform  = $evm.root['platform']
-$evm.log("info", "Detected Platform:<#{platform}>")
+              # Set run_env_dialog to true to dynamically choose dialog name based on environment tag
+              def main
+                platform = @handle.root['platform']
+                @handle.log("info", "Detected Platform:<#{platform}>")
 
-if platform.nil?
-  source_id = $evm.root['dialog_input_src_vm_id']
-  source    = $evm.vmdb('vm_or_template', source_id) unless source_id.nil?
-  if source
-    platform = source.model_suffix.downcase
-  else
-    platform = "vmware"
+                if platform.nil?
+                  source_id = @handle.root['dialog_input_src_vm_id']
+                  source    = @handle.vmdb('vm_or_template', source_id) unless source_id.nil?
+                  platform  = source ? source.model_suffix.downcase : "vmware"
+                end
+
+                dialog_name_prefix = "miq_provision_#{platform}_dialogs"
+                @handle.object['dialog_name_prefix'] = dialog_name_prefix
+                @handle.log("info", "Platform:<#{platform}> dialog_name_prefix:<#{dialog_name_prefix}>")
+              end
+            end
+          end
+        end
+      end
+    end
   end
 end
 
-dialog_name_prefix = "miq_provision_#{platform}_dialogs"
-$evm.object['dialog_name_prefix'] = dialog_name_prefix
-$evm.log("info", "Platform:<#{platform}> dialog_name_prefix:<#{dialog_name_prefix}>")
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Cloud::VM::Provisioning::Profile::VmDialogNamePrefix.new.main
+end
---
+++ spec/content/automate/ManageIQ/Cloud/VM/Provisioning/Profile.class/__methods__/vm_dialog_name_prefix_spec.rb
@@ -0,0 +1,35 @@
+require_domain_file
+
+describe ManageIQ::Automate::Cloud::VM::Provisioning::Profile::VmDialogNamePrefix do
+  let(:platform)        { 'test_platform' }
+  let(:root_hash)       { { 'platform' => platform } }
+  let(:root_object)     { Spec::Support::MiqAeMockObject.new(root_hash) }
+  let(:vm_template)     { FactoryGirl.create(:template_amazon) }
+  let(:svc_vm_template) { MiqAeMethodService::MiqAeServiceVmOrTemplate.find(vm_template.id) }
+
+  let(:ae_service) do
+    Spec::Support::MiqAeMockService.new(root_object).tap do |service|
+      current_object = Spec::Support::MiqAeMockObject.new
+      current_object.parent = root_object
+      service.object = current_object
+    end
+  end
+
+  it "sets dialog_name_prefix from 'root['platform']'" do
+    described_class.new(ae_service).main
+    expect(ae_service.object['dialog_name_prefix']).to(eq("miq_provision_#{platform}_dialogs"))
+  end
+
+  it 'sets dialog_name_prefix from vmdb' do
+    ae_service.root['platform'] = nil
+    ae_service.root['dialog_input_src_vm_id'] = svc_vm_template.id
+    described_class.new(ae_service).main
+    expect(ae_service.object['dialog_name_prefix']).to(eq("miq_provision_#{svc_vm_template.model_suffix.downcase}_dialogs"))
+  end
+
+  it 'sets default dialog_name_prefix' do
+    ae_service.root['platform'] = nil
+    described_class.new(ae_service).main
+    expect(ae_service.object['dialog_name_prefix']).to(eq("miq_provision_vmware_dialogs"))
+  end
+end
