fb6f12ceff23a21bbdb891431ff31b43820bc8ca
Greg McCullough <gmccullo@redhat.com>
2018-07-19 08:06:54 -0400

    Merge pull request #355 from fdupont-redhat/v2v_pre_post_migration_playbook_support
    Add support for pre/post-migration playbook
    (cherry picked from commit f5fdc8293ac754a37674920607d7743392d94fcd)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1608420

---
+++ content/automate/ManageIQ/Transformation/Ansible.class/__methods__/launchplaybookasaservice.rb
@@ -0,0 +1,45 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Ansible
+        class LaunchPlaybookAsAService
+          def initialize(handle = $evm)
+            @handle = handle
+          end
+
+          def target_host(task, transformation_hook)
+            target_host = nil
+            case transformation_hook
+            when 'pre'
+              target_host = task.source
+            when 'post'
+              target_host = @handle.vmdb(:vm).find_by(:id => task.get_option(:destination_vm_id))
+            end
+            target_host
+          end
+
+          def main
+            task = @handle.root['service_template_transformation_plan_task']
+            transformation_hook = @handle.inputs['transformation_hook']
+
+            return if transformation_hook == '_'
+            service_template = task.send("#{transformation_hook}_ansible_playbook_service_template")
+            return if service_template.nil?
+            target_host = target_host(task, transformation_hook)
+            return if target_host.nil? || target_host.power_state != 'on'
+            service_dialog_options = { :hosts => target_host.ipaddresses.first }
+            service_request = @handle.execute(:create_service_provision_request, service_template, service_dialog_options)
+            task.set_option("#{transformation_hook}_ansible_playbook_service_request_id".to_sym, service_request.id)
+          rescue => e
+            @handle.set_state_var(:ae_state_progress, 'message' => e.message)
+            raise
+          end
+        end
+      end
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Transformation::Ansible::LaunchPlaybookAsAService.new.main
+end
