3d43a639999bb0194e34c8a80a7d6f360dfaa32e
Greg McCullough <gmccullo@redhat.com>
2018-07-31 20:48:51 -0400

    Merge pull request #382 from fdupont-redhat/v2v_fix_cleanup_state_machine_poweron
    Fix default cleanup state machine
    (cherry picked from commit 3e6a3f656cac57ad1b9ec2c1a6d1e8cdde44e6f4)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1608758

---
+++ content/automate/ManageIQ/Transformation/Infrastructure/VM/Common.class/__methods__/checkpoweredon.rb
@@ -0,0 +1,41 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Infrastructure
+        module VM
+          module Common
+            class CheckPoweredOn
+              def initialize(handle = $evm)
+                @handle = handle
+              end
+
+              def main
+                if @handle.root['service_template_transformation_plan_task'].blank?
+                  task = @handle.vmdb(:service_template_transformation_plan_task).find_by(:id => @handle.root['service_template_transformation_plan_task_id'])
+                  vm = task.source if task.present?
+                else
+                  task = @handle.root['service_template_transformation_plan_task']
+                  vm = @handle.vmdb(:vm).find_by(:id => task.get_option(:destination_vm_id)) if task.present?
+                end
+                return if vm.blank?
+                @handle.log(:info, "Target VM: #{vm.name} [#{vm.vendor}]")
+                return if task.get_option(:source_vm_power_state) != 'on'
+                if vm.power_state != 'on'
+                  @handle.root["ae_result"] = "retry"
+                  @handle.root["ae_retry_interval"] = "15.seconds"
+                end
+              rescue => e
+                @handle.set_state_var(:ae_state_progress, 'message' => e.message)
+                raise
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  ManageIQ::Automate::Transformation::Infrastructure::VM::Common::CheckPoweredOn.new.main
+end
