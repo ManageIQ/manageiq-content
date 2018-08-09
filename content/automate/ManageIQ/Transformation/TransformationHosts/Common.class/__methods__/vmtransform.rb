8310264ecfe3beb70874a92a84633f2ca96daefd
Fabien Dupont <fdupont@redhat.com>
2018-08-09 10:50:14 -0400

    Apply PR#390 recommendation

--- content/automate/ManageIQ/Transformation/TransformationHosts/Common.class/__methods__/vmtransform.rb
+++ content/automate/ManageIQ/Transformation/TransformationHosts/Common.class/__methods__/vmtransform.rb
@@ -53,6 +53,4 @@ module ManageIQ
   end
 end
 
-if  == __FILE__
-  ManageIQ::Automate::Transformation::TransformationHost::Common::VMTransform.new.main
-end
+ManageIQ::Automate::Transformation::TransformationHost::Common::VMTransform.new.main
