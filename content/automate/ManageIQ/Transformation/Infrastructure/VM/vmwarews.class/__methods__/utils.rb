6f40854d757a87da24eebc2ebb402c58de6de640
Fabien Dupont <fdupont@redhat.com>
2018-09-26 11:24:51 -0400

    Removing unused code that should belong to the backend model

--- content/automate/ManageIQ/Transformation/Infrastructure/VM/vmwarews.class/__methods__/utils.rb
+++ content/automate/ManageIQ/Transformation/Infrastructure/VM/vmwarews.class/__methods__/utils.rb
@@ -21,31 +21,6 @@ module ManageIQ
 
                 Digest::SHA1.hexdigest(cert.to_der).upcase.scan(/../).join(":")
               end
-
-              def self.provider_connection(ems)
-                @provider_connection ||= RbVmomi::VIM.connect(:host => ems.ipaddress || ems.hostname, :user => ems.authentication_userid, :password => ems.authentication_password, :insecure => true)
-              rescue => e
-                raise "Could not connect to #{ems.name}: #{e.message}" if c.nil?
-              end
-              private_class_method :provider_connection
-
-              def self.vm_get_ref(vim, vm)
-                dc = vim.serviceInstance.find_datacenter(vm.v_owning_datacenter)
-                raise "Datacenter '#{vm.datacenter.name}' not found in vCenter" unless dc
-                vim.serviceInstance.content.searchIndex.FindByUuid(:datacenter => dc, :uuid => vm.uid_ems, :vmSearch => true, :instanceUuid => false)
-              rescue => e
-                raise "Could not find VM #{vm.name}: #{e.message}"
-              end
-              private_class_method :vm_get_ref
-
-              def self.vm_rename(vm, new_name, handle = $evm)
-                vim_vm = vm_get_ref(provider_connection, vm)
-                raise "Unable to locate #{vm.name} in datacenter #{dc}" unless vim_vm
-                vim_vm.ReconfigVM_Task(:spec => RbVmomi::VIM::VirtualMachineConfigSpec(:name=> new_name)).wait_for_completion
-                dc.find_vm(options[:new_name])
-              rescue => e
-                raise "Failed to rename #{options[:source_vm]}: #{e.message}"
-              end
             end
           end
         end
