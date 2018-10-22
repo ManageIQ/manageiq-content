b04303f7fef19db281e18283bca292dce18bfd65
Lucy Fu <lufu@redhat.com>
2018-10-22 11:54:25 -0400

    Fix travis failure.
    Followup of https://github.com/ManageIQ/manageiq/pull/18057

--- spec/content/automate/ManageIQ/AutomationManagement/AnsibleTower/Service/Provisioning/StateMachines/Provision.class/__methods__/provision_spec.rb
+++ spec/content/automate/ManageIQ/AutomationManagement/AnsibleTower/Service/Provisioning/StateMachines/Provision.class/__methods__/provision_spec.rb
@@ -6,12 +6,14 @@ describe ManageIQ::Automate::AutomationManagement::AnsibleTower::Service::Provis
   let(:job_class) { ManageIQ::Providers::AnsibleTower::AutomationManager::Job }
   let(:ansible_tower_manager) { FactoryGirl.create(:automation_manager) }
   let(:job_template) { FactoryGirl.create(:ansible_configuration_script, :manager => ansible_tower_manager) }
-  let(:service_ansible_tower) { FactoryGirl.create(:service_ansible_tower, :job_template => job_template) }
+  let(:service_ansible_tower) { FactoryGirl.create(:service_ansible_tower, :job_template => job_template, :evm_owner => FactoryGirl.create(:user_with_group)) }
   let(:job) { FactoryGirl.create(:ansible_tower_job) }
   let(:task) { FactoryGirl.create(:service_template_provision_task, :destination => service_ansible_tower, :miq_request => request) }
   let(:svc_task) { MiqAeMethodService::MiqAeServiceServiceTemplateProvisionTask.find(task.id) }
   let(:root_object) { Spec::Support::MiqAeMockObject.new('service_template_provision_task' => svc_task) }
   let(:ae_service) { Spec::Support::MiqAeMockService.new(root_object) }
+
+  before { FactoryGirl.create(:miq_region, :region => ApplicationRecord.my_region_number) }
 
   it "launches an Ansible Tower job" do
     expect(job_class).to receive(:create_job).and_return(job)
