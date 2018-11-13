c4ce40ad4f060bce13b907ee32771fd01279ba6e
Greg McCullough <gmccullo@redhat.com>
2018-11-13 10:25:29 -0500

    Merge pull request #471 from fdupont-redhat/v2v_fix_request_denial
    Fix request denial
    
    (cherry picked from commit 4e2e22969181d60b5e1fd76d0db6da4f46df7313)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1640816

--- content/automate/ManageIQ/Transformation/StateMachines/TransformationPlanRequestApproval.class/__methods__/deny_request.rb
+++ content/automate/ManageIQ/Transformation/StateMachines/TransformationPlanRequestApproval.class/__methods__/deny_request.rb
@@ -2,4 +2,4 @@ request = $evm.root['miq_request']
 message = $evm.object['reason']
 $evm.log('info', "Request denied because of #{message}")
 request.message = message
-request.deny('admin', msg = message)
+request.deny('admin', message)
