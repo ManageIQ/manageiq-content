97a172ed478f8ea9453bd3cb87cb984b3f09bdd9
James Wong <jwong@redhat.com>
2019-01-24 12:35:41 -0500

    Remove throttling which are now done in backend

--- content/automate/ManageIQ/Transformation/Common.class/__methods__/preflightcheck.rb
+++ content/automate/ManageIQ/Transformation/Common.class/__methods__/preflightcheck.rb
@@ -2,21 +2,19 @@ module ManageIQ
   module Automate
     module Transformation
       module Common
-        class AcquireTransformationHost
+        class PreflightCheck
           def initialize(handle = $evm)
             @handle = handle
             @task = ManageIQ::Automate::Transformation::Common::Utils.task(@handle)
           end
 
           def main
-            if @task.conversion_host.nil?
+            @handle.log(:info, "PreflightCheck: task.state: #{@task.state}")
+            if @task.state != 'migrate'
               @handle.root['ae_result'] = 'retry'
               @handle.root['ae_retry_server_affinity'] = true
               @handle.root['ae_retry_interval'] = 15.seconds
             end
-          rescue => e
-            @handle.set_state_var(:ae_state_progress, 'message' => e.message)
-            raise
           end
         end
       end
@@ -24,4 +22,4 @@ module ManageIQ
   end
 end
 
-ManageIQ::Automate::Transformation::Common::AcquireTransformationHost.new.main
+ManageIQ::Automate::Transformation::Common::PreflightCheck.new.main
