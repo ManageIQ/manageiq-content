267b4786c5231afd17a445ba4aafa0f0670de38f
Greg McCullough <gmccullo@redhat.com>
2019-10-10 10:08:17 -0400

    Merge pull request #578 from pkomanek/eligible_storages_for_vmware_best_fit_least_utilized
    Eligible storages for placement methods
    
    (cherry picked from commit 957bbd973e3d0856c426004b8adb99eeb3d82659)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1805979

--- content/automate/ManageIQ/Infrastructure/VM/Provisioning/Placement.class/__methods__/microsoft_best_fit_least_utilized.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Provisioning/Placement.class/__methods__/microsoft_best_fit_least_utilized.rb
@@ -2,6 +2,10 @@
 # Description: This method is used to find all hosts, datastores that are the least utilized
 #
 
+def clear_host(prov)
+  prov.set_option(:placement_host_name, [nil, nil])
+end
+
 prov = $evm.root["miq_provision"]
 vm   = prov.vm_template
 raise "VM not specified" if vm.nil?
@@ -19,8 +23,12 @@ prov.eligible_hosts.each do |h|
   next unless h.power_state == "on"
   nvms = h.vms.length
 
+  # Setting the host to filter eligible storages
+  prov.set_host(h)
+
   if min_registered_vms.nil? || nvms < min_registered_vms
-    storages = h.writable_storages.find_all { |s| s.free_space > vm.provisioned_storage } # Filter out storages that do not have enough free space for the Vm
+    # Filter out storages that do not have enough free space for the Vm
+    storages = prov.eligible_storages.find_all { |s| s.free_space > vm.provisioned_storage }
     s = storages.max_by(&:free_space)
     unless s.nil?
       host    = h
@@ -31,7 +39,7 @@ prov.eligible_hosts.each do |h|
 end
 
 # Set host and storage
-prov.set_host(host) if host
+host ? prov.set_host(host) : clear_host(prov)
 prov.set_storage(storage) if storage
 
 $evm.log("info", "vm=[#{vm.name}] host=[#{host.try(:name)}] storage=[#{storage.try(:name)}]")
