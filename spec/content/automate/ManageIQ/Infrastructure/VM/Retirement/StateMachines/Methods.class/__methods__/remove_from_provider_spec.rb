da8b70b27edb36e674cd9e350025a0c45ef5a394
William Fitzgerald <wfitzger@redhat.com>
2019-11-04 16:10:12 -0500

    Refactor remove_from_provider method for Infra VM Retirement.
    This PR is based on the issue below.
    ManageIQ/manageiq#12038
    
    @miq-bot add_label refactoring
    @miq-bot assign @tinaafitz
    Added test for 'unregister' which was missing in coverage

---
+++ spec/content/automate/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/remove_from_provider_spec.rb
@@ -0,0 +1,75 @@
+require_domain_file
+
+describe ManageIQ::Automate::Infrastructure::VM::Retirement::StateMachines::Methods::RemoveFromProvider do
+  let(:zone)        { FactoryBot.create(:zone) }
+  let(:svc_vm)      { MiqAeMethodService::MiqAeServiceVm.find(vm.id) }
+  let(:root_hash)   { { 'vm' => svc_vm } }
+  let(:root_object) { Spec::Support::MiqAeMockObject.new(root_hash) }
+
+  let(:ae_service) do
+    Spec::Support::MiqAeMockService.new(root_object).tap do |service|
+      current_object = Spec::Support::MiqAeMockObject.new
+      current_object.parent = root_object
+      service.object = current_object
+    end
+  end
+
+  shared_examples 'ae_method' do
+    it "without removal_type" do
+      expect(ae_service).to receive(:log).with('info', "Unknown retirement type for VM:<#{vm.name}> from provider:<#{ems.name}>")
+      expect { described_class.new(ae_service).main }.to raise_error('Unknown retirement type')
+      expect(ae_service.get_state_var('vm_removed_from_provider')).to be_falsey
+    end
+
+    it "vm without tags" do
+      ae_service.inputs['removal_type'] = 'remove_from_disk'
+      described_class.new(ae_service).main
+      expect(ae_service.get_state_var('vm_removed_from_provider')).to be_falsey
+    end
+
+    it "nil vm" do
+      ae_service.root['vm'] = nil
+      expect(ae_service).to receive(:log).with('info', 'Skipping remove from provider for Instance:<> on provider:<>')
+      described_class.new(ae_service).main
+      expect(ae_service.get_state_var('vm_removed_from_provider')).to be_falsey
+    end
+
+    it "removes a vm" do
+      ae_service.inputs['removal_type'] = 'remove_from_disk'
+      vm.tag_with("retire_full", :ns => "/managed", :cat => "lifecycle")
+      expect { described_class.new(ae_service).main }.to_not raise_error
+      expect(ae_service.get_state_var('vm_removed_from_provider')).to be_truthy
+    end
+
+    it "unregister a vm" do
+      ae_service.inputs['removal_type'] = 'unregister'
+      expect(ae_service).to receive(:log).with('info', "Unregistering Instance:<#{vm.name}> from provider:<#{ems.name}>")
+      expect { described_class.new(ae_service).main }.to_not raise_error
+      expect(ae_service.get_state_var('vm_removed_from_provider')).to be_truthy
+    end
+
+    context "without ems" do
+      let(:vm) { FactoryBot.create(:vm_vmware) }
+
+      it "skips removing" do
+        expect(ae_service).to receive(:log).with('info', "Skipping remove from provider for Instance:<#{svc_vm.try(:name)}> on provider:<>")
+        described_class.new(ae_service).main
+        expect(ae_service.get_state_var('vm_removed_from_provider')).to be_falsey
+      end
+    end
+  end
+
+  context "Infrastructure" do
+    let(:ems) { FactoryBot.create(:ems_vmware, :zone => zone) }
+    let(:vm) { FactoryBot.create(:vm_vmware, :ems_id => ems.id) }
+
+    it_behaves_like 'ae_method'
+  end
+
+  context "Cloud" do
+    let(:ems) { FactoryBot.create(:ems_google, :zone => zone) }
+    let(:vm) { FactoryBot.create(:vm_google, :ems_id => ems.id) }
+
+    it_behaves_like 'ae_method'
+  end
+end
