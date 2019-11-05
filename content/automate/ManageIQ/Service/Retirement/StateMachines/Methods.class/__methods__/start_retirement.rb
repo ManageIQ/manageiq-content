b3f636a2eaa57c0d44194efb3b36b1a9cf9fc8fa
William Fitzgerald <wfitzger@redhat.com>
2019-11-05 12:11:42 -0500

    Refactor start_retirement method for Service Retirement.
    This PR is based on the issue below.
    ManageIQ/manageiq#12038
    
    @miq-bot add_label refactoring
    @miq-bot assign @tinaafitz
    
    Added test for service_retire_task = nil, was missing this in coverage
    Added test for retirement_state = 'retiring' as requested
    
    Modified method as requested
    Deleted comment and blank line
    Removed extra log messages because Raise doesn't need them
    Fixed rob error
    Fixed raise logic to 1 line

--- content/automate/ManageIQ/Service/Retirement/StateMachines/Methods.class/__methods__/start_retirement.rb
+++ content/automate/ManageIQ/Service/Retirement/StateMachines/Methods.class/__methods__/start_retirement.rb
@@ -1,32 +1,57 @@
 #
 # Description: This method sets the retirement_state to retiring
 #
+module ManageIQ
+  module Automate
+    module Service
+      module Retirement
+        module StateMachines
+          module Methods
+            class StartRetirement
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-service = $evm.root['service']
-if service.nil?
-  $evm.log('error', "Service Object not found")
-  exit MIQ_ABORT
+              def main
+                @handle.log('info', "Service before start_retirement: #{service.inspect} ")
+                start_retirement(service)
+                @handle.log('info', "Service after start_retirement: #{service.inspect} ")
+              end
+
+              private
+
+              def service
+                @handle.root["service"].tap do |service|
+                  raise 'Service Object not found' if service.nil?
+                end
+              end
+
+              def start_retirement(service)
+                service_validation(service)
+                @handle.create_notification(:type => :service_retiring, :subject => service)
+
+                service.start_retirement
+              end
+
+              def service_validation(service)
+                if service.retired?
+                  raise 'Service is already retired'
+                end
+
+                if service.retiring?
+                  raise 'Service is already in the process of being retired'
+                end
+
+                unless @handle.root['service_retire_task']
+                  raise 'Service retire task not found, The old style retirement is incompatible with the new retirement state machine.'
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
-$evm.log('info', "Service before start_retirement: #{service.inspect} ")
-
-if service.retired?
-  $evm.log('error', "Service is already retired. Aborting current State Machine.")
-  exit MIQ_ABORT
-end
-
-if service.retiring?
-  $evm.log('error', "Service is in the process of being retired. Aborting current State Machine.")
-  exit MIQ_ABORT
-end
-
-unless $evm.root['service_retire_task']
-  $evm.log(:error, "Service retire task not found")
-  $evm.log(:error, "The old style retirement is incompatible with the new retirement state machine.")
-  exit(MIQ_ABORT)
-end
-
-$evm.create_notification(:type => :service_retiring, :subject => service)
-service.start_retirement
-
-$evm.log('info', "Service after start_retirement: #{service.inspect} ")
+ManageIQ::Automate::Service::Retirement::StateMachines::Methods::StartRetirement.new.main
