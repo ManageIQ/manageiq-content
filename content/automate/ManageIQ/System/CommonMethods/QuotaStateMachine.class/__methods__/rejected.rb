dcb1a2f6dcb46fbea763e0ca7107d28194dbfb38
Greg Piatigorski <gpiatigo@gmail.com>
2019-11-06 14:23:43 -0800

    Refactor AE Content Rejected method and add spec

--- content/automate/ManageIQ/System/CommonMethods/QuotaStateMachine.class/__methods__/rejected.rb
+++ content/automate/ManageIQ/System/CommonMethods/QuotaStateMachine.class/__methods__/rejected.rb
@@ -1,9 +1,36 @@
+# frozen_string_literal: true
+
 #
 # Description: Quota Exceeded rejected method.
 #
+module ManageIQ
+  module Automate
+    module System
+      module CommonMethods
+        module QuotaStateMachine
+          class Rejected
+            def initialize(handle = $evm)
+              @handle = handle
+            end
 
-request = $evm.root['miq_request']
-$evm.log('info', "Request denied because of #{request.message}")
-request.deny('admin', 'Quota Exceeded')
+            def main
+              rejected
+            end
 
-$evm.create_notification(:level => 'error', :subject => request, :message => "Quota Exceeded: #{request.message}")
+            private
+
+            def rejected
+              request = @handle.root['miq_request']
+              @handle.log('info', "Request denied because of #{request.message}")
+              request.deny('admin', 'Quota Exceeded')
+
+              @handle.create_notification(:level => 'error', :subject => request, :message => "Quota Exceeded: #{request.message}")
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+ManageIQ::Automate::System::CommonMethods::QuotaStateMachine::Rejected.new.main
