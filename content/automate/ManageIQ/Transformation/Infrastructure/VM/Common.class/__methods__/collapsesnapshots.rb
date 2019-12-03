ef852cadf851d2835424c46ccf8db2fef62e2074
Satoe Imaishi <simaishi@redhat.com>
2019-12-03 11:13:32 -0500

    Revert "Merge pull request #566 from fdupont-redhat/v2v_state_machine_remove_collapse_snapshots"
    This reverts commit f8d1c45aff1132f2b10daa7b84c9afe802001c22.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1767543

---
+++ content/automate/ManageIQ/Transformation/Infrastructure/VM/Common.class/__methods__/collapsesnapshots.rb
@@ -0,0 +1,30 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Infrastructure
+        module VM
+          module VMware
+            class CollapseSnapshots
+              def initialize(handle = $evm)
+                @handle = handle
+                @task = ManageIQ::Automate::Transformation::Common::Utils.task(@handle)
+                @source_vm = ManageIQ::Automate::Transformation::Common::Utils.source_vm(@handle)
+              end
+
+              def main
+                return unless @source_vm.vendor == 'vmware'
+                return if @source_vm.snapshots.empty?
+                @source_vm.remove_all_snapshots
+              rescue => e
+                @handle.set_state_var(:ae_state_progress, 'message' => e.message)
+                raise
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+ManageIQ::Automate::Transformation::Infrastructure::VM::VMware::CollapseSnapshots.new.main
