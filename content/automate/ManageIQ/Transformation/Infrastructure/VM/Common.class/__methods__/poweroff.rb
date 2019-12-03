913b4fe437777abe6f117c960b9f3f922a3b4b2e
Satoe Imaishi <simaishi@redhat.com>
2019-12-03 11:13:01 -0500

    Revert "Merge pull request #568 from fdupont-redhat/v2v_state_machine_shutdown"
    This reverts commit 1f4748a522a81d77ceb4d9800e85c81e3efb3b5e.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1767545

---
+++ content/automate/ManageIQ/Transformation/Infrastructure/VM/Common.class/__methods__/poweroff.rb
@@ -0,0 +1,35 @@
+module ManageIQ
+  module Automate
+    module Transformation
+      module Infrastructure
+        module VM
+          module Common
+            class PowerOff
+              def initialize(handle = $evm)
+                @handle = handle
+                @task = ManageIQ::Automate::Transformation::Common::Utils.task(@handle)
+                @source_vm = ManageIQ::Automate::Transformation::Common::Utils.source_vm(@handle)
+              end
+
+              def main
+                return if @source_vm.power_state == 'off'
+                if @handle.state_var_exist?(:vm_shutdown_in_progress)
+                  @source_vm.stop if @handle.root['ae_state_retries'].to_i > 10
+                else
+                  @source_vm.shutdown_guest
+                  @handle.set_state_var(:vm_shutdown_in_progress, true)
+                end
+                @handle.root['ae_result'] = 'retry'
+              rescue => e
+                @handle.set_state_var(:ae_state_progress, 'message' => e.message)
+                raise
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+ManageIQ::Automate::Transformation::Infrastructure::VM::Common::PowerOff.new.main
