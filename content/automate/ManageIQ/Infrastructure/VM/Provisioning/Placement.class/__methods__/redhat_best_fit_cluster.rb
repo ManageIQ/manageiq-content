a9b9e8b78d53aed9c45f57fad793822096cb2d0f
William Fitzgerald <wfitzger@redhat.com>
2020-01-14 16:52:56 -0500

    Refactor redhat_best_fit_cluster method for VM Placement.
    Refactored method and created test.
    
    This PR is based on the issue below.
    ManageIQ/manageiq#12038
    
    @miq-bot add_label refactoring
    @miq-bot assign @tinaafitz
    Fixed description
    Changed ems logic in method as requested

--- content/automate/ManageIQ/Infrastructure/VM/Provisioning/Placement.class/__methods__/redhat_best_fit_cluster.rb
+++ content/automate/ManageIQ/Infrastructure/VM/Provisioning/Placement.class/__methods__/redhat_best_fit_cluster.rb
@@ -1,22 +1,53 @@
 #
 # Description: This method sets the cluster based on source template
 #
+module ManageIQ
+  module Automate
+    module Infrastructure
+      module VM
+        module Provisioning
+          module Placement
+            class RedhatBestFitCluster
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-# Get variables
-prov = $evm.root["miq_provision"]
-vm = prov.vm_template
-raise "VM not specified" if vm.nil?
-user = prov.miq_request.requester
-raise "User not specified" if user.nil?
-ems  = vm.ext_management_system
-raise "EMS not found for VM [#{vm.name}" if ems.nil?
+              def main
+                best_fit_cluster
+              end
 
-$evm.log("info", "vm=[#{vm.name}]")
+              private
 
-cluster = vm.ems_cluster
-$evm.log("info", "Selected Cluster: [#{cluster.nil? ? "nil" : cluster.name}]")
+              def request
+                @handle.root["miq_provision"].tap do |req|
+                  raise "miq_provision not specified" if req.nil?
+                end
+              end
 
-# Set cluster
-prov.set_cluster(cluster) if cluster
+              def vm
+                request.vm_template.tap do |vm|
+                  raise 'VM not specified' if vm.nil?
+                  raise "EMS not found for VM [#{vm.name}]" if vm.ext_management_system.nil?
+                end
+              end
 
-$evm.log("info", "vm=[#{vm.name}] cluster=[#{cluster}]")
+              def best_fit_cluster
+                @handle.log("info", "vm=[#{vm.name}]")
+
+                cluster = vm.ems_cluster
+                @handle.log("info", "Selected Cluster: [#{cluster.nil? ? "nil" : cluster.name}]")
+
+                # Set cluster
+                request.set_cluster(cluster) if cluster
+
+                @handle.log("info", "vm=[#{vm.name}] cluster=[#{cluster}]")
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+ManageIQ::Automate::Infrastructure::VM::Provisioning::Placement::RedhatBestFitCluster.new.main
