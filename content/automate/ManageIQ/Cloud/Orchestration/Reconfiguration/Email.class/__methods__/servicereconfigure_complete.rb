26f28bc708af85e1208df078ea2a25fa866bb6cb
William Fitzgerald <wfitzger@redhat.com>
2020-02-24 16:29:59 -0500

    Removed all deprecated email components from Cloud namespace. Removed class and all instances and methods from 4 classes:
    Cloud/Orchestration/Provisioning/Email
    Cloud/Orchestration/Reconfigure/Email
    Cloud/Orchestration/Retirement/Email
    
    Cloud/VM/Provisioning/Email
    Cloud/VM/Retirement/Email
    
    Email is now in System/Notification/Email class.
    
    @miq-bot add_label hammer/no, ivanchuk/no, changelog/yes, technical debt
    Removed vm_retire_extend_spec.rb

--- content/automate/ManageIQ/Cloud/Orchestration/Reconfiguration/Email.class/__methods__/servicereconfigure_complete.rb
+++
@@ -1,4 +0,0 @@
-#
-# Description: Place holder for Service Reconfigure Complete email #
-#
-$evm.log("warn", "[DEPRECATION] This method will be deprecated. Please use similarly named method from System/Notification/Email namespace.")
