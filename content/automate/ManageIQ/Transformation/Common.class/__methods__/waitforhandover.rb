1b00d78cdf7fe953b8c4dea7605aef5141d11a78
tina <tfitzger@redhat.com>
2020-04-06 10:52:50 -0400

    Merge pull request #651 from fdupont-redhat/bz_1819998
    Fix unhandled task failure during preflight check
    
    (cherry picked from commit b46ada57ad122a1e2ba92717f5b436282d027959)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1819998

--- content/automate/ManageIQ/Transformation/Common.class/__methods__/waitforhandover.rb
+++ content/automate/ManageIQ/Transformation/Common.class/__methods__/waitforhandover.rb
@@ -10,7 +10,7 @@ module ManageIQ
 
           def main
             @handle.log(:info, "WaitForHandover: task.state: #{@task.state} - task.options.workflow_runner: #{@task.get_option(:workflow_runner)}")
-            if @task.state != 'migrate' || @task.get_option(:workflow_runner) != 'automate'
+            if %w[active migrate].exclude?(@task.state) || @task.get_option(:workflow_runner) != 'automate'
               @handle.root['ae_result'] = 'retry'
               @handle.root['ae_retry_server_affinity'] = true
               @handle.root['ae_retry_interval'] = 15.seconds
