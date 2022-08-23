c7fbaca341af781b19f7eaaeb94968f4802722ca
eilam <eilam.elimelech@kyndryl.com>
2022-08-23 13:20:04 +0300

    fix

--- content/automate/ManageIQ/Storage/Autosde/Services/ServiceVolumeRequestApproval.class/__methods__/approve_request.rb
+++ content/automate/ManageIQ/Storage/Autosde/Services/ServiceVolumeRequestApproval.class/__methods__/approve_request.rb
@@ -8,11 +8,13 @@ size = $evm.root.attributes['dialog_size']
 if size == 'custom'
   size = $evm.root.attributes['dialog_custom']
 end
-options={
-  "name"=>$evm.root.attributes['dialog_name'],
-  "size"=>size, "count"=>1,
-  "storage_service_id"=>$evm.root.attributes['dialog_pool']}
+options = {
+  "name" => $evm.root.attributes['dialog_name'],
+  "size" => size,
+  "count" => 1,
+  "storage_service_id" => $evm.root.attributes['dialog_pool']
+}
 
-cv =$evm.vmdb(:cloud_volume)
+cv = $evm.vmdb(:cloud_volume)
 
-cv.create_volume_task(ems.id,options)
+cv.create_volume_task(ems.id, options)
