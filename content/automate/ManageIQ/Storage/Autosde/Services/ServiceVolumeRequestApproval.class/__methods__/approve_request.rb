22a335639c49326e9b915e8483cf18ef177102b9
eilam <eilam.elimelech@kyndryl.com>
2022-08-23 12:59:29 +0300

    use the cloud volume class

--- content/automate/ManageIQ/Storage/Autosde/Services/ServiceVolumeRequestApproval.class/__methods__/approve_request.rb
+++ content/automate/ManageIQ/Storage/Autosde/Services/ServiceVolumeRequestApproval.class/__methods__/approve_request.rb
@@ -1,22 +1,18 @@
 # Create a volume after service request
-ems = $evm.vmdb(:ext_management_system).where(:id=>$evm.root.attributes['dialog_ems']).first
-$evm.log("info", 'creating a volume after order request.')
 
-autosde_client = ems.object_send('autosde_client')
+$evm.log("info", 'creating a volume after order request.')
+ems = $evm.vmdb(:ext_management_system).where(:id=>$evm.root.attributes['dialog_ems']).first
 
 size = $evm.root.attributes['dialog_size']
 
 if size == 'custom'
   size = $evm.root.attributes['dialog_custom']
 end
+options={
+  "name"=>$evm.root.attributes['dialog_name'],
+  "size"=>size, "count"=>1,
+  "storage_service_id"=>$evm.root.attributes['dialog_pool']}
 
-vol_to_create = autosde_client.VolumeCreate(
-  :service => $evm.root.attributes['dialog_pool'],
-  :name    => $evm.root.attributes['dialog_name'],
-  :size    => size,
-  :count   => 1
-)
+cv =$evm.vmdb(:cloud_volume)
 
-autosde_client.VolumeApi.volumes_post(vol_to_create)
-ems.refresh
-exit MIQ_OK
+cv.create_volume_task(ems.id,options)
