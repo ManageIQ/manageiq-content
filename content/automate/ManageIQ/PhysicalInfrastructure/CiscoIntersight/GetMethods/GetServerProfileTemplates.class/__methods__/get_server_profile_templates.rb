dee4ef4405f18f4104d469291bfbaec5207ad07e
Tzur Vaza <tzurvaza@gmail.com>
2022-11-24 13:32:31 +0200

    Update get_server_profile_templates.rb

--- content/automate/ManageIQ/PhysicalInfrastructure/CiscoIntersight/GetMethods/GetServerProfileTemplates.class/__methods__/get_server_profile_templates.rb
+++ content/automate/ManageIQ/PhysicalInfrastructure/CiscoIntersight/GetMethods/GetServerProfileTemplates.class/__methods__/get_server_profile_templates.rb
@@ -8,7 +8,7 @@ selected_ems_id = $evm.root.attributes['dialog_ems']
 selected_ems = $evm.vmdb(:ext_management_system).where(:id=>selected_ems_id).first
 
 templates = selected_ems.physical_server_profile_templates
-templates_l = templates.map { |t| {t.ems_ref => t.name} }
+templates_l = templates.map { |t| {t.id => t.name} }
 dialog_field["values"] = Hash[*templates_l.map(&:to_a).flatten]
 
 exit MIQ_OK
