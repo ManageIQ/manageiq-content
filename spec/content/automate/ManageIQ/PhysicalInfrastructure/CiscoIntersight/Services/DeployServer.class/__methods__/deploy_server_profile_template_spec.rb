102a64345da19815197bbcd14735bb95f54fb124
eilam <eilam.elimelech@kyndryl.com>
2023-02-09 12:56:26 +0200

    add expects to the tests

--- spec/content/automate/ManageIQ/PhysicalInfrastructure/CiscoIntersight/Services/DeployServer.class/__methods__/deploy_server_profile_template_spec.rb
+++ spec/content/automate/ManageIQ/PhysicalInfrastructure/CiscoIntersight/Services/DeployServer.class/__methods__/deploy_server_profile_template_spec.rb
@@ -3,13 +3,13 @@ require_domain_file
 describe ManageIQ::Automate::PhysicalInfrastructure::CiscoIntersight::Services::DeployServer::Methods::DeployServerProfileTemplate do
   let(:ems) { FactoryBot.create(:ems_cisco_intersight_physical_infra, :auth) }
 
-  let(:physical_server_profile_template) { FactoryBot.create(:physical_server_profile_template, :ext_management_system=>ems) }
+  let(:physical_server_profile_template) { FactoryBot.create(:physical_server_profile_template, :ext_management_system => ems) }
 
-  let(:physical_server_profile_template_second) { FactoryBot.create(:physical_server_profile_template, :ext_management_system=>ems) }
+  let(:physical_server_profile_template_second) { FactoryBot.create(:physical_server_profile_template, :ext_management_system => ems) }
 
   let(:service_template) { FactoryBot.create(:service_template, :options => {:server_profile_template_id => physical_server_profile_template.id}) }
 
-  let(:task) { FactoryBot.create(:service_template_provision_task, :source=>service_template) }
+  let(:task) { FactoryBot.create(:service_template_provision_task, :source => service_template) }
 
   let(:root_hash) do
     {
@@ -32,10 +32,19 @@ describe ManageIQ::Automate::PhysicalInfrastructure::CiscoIntersight::Services::
 
   it "with server profile template pre selected" do
     described_class.new(ae_service).main
+    queue_item = MiqQueue.first
+    expect(queue_item.method_name).to eq("deploy_server_from_template")
+    expect(queue_item.class_name).to eq("PhysicalServerProfileTemplate")
+    expect(queue_item.instance_id).to eq(physical_server_profile_template.id)
+
   end
 
   it "without server profile template pre selected" do
     service_template.options[:server_profile_template_id] = nil
     described_class.new(ae_service).main
+    queue_item = MiqQueue.first
+    expect(queue_item.method_name).to eq("deploy_server_from_template")
+    expect(queue_item.class_name).to eq("PhysicalServerProfileTemplate")
+    expect(queue_item.instance_id).to eq(physical_server_profile_template_second.id)
   end
 end
